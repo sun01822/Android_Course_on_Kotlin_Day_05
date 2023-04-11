ed7467350cf9ef1200aa90a65c6a3036db94107c
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-04-12 03:04:17 +0600

    Second Commit

--- app/src/main/java/com/example/design_a_layout/MainActivity2.kt
+++ app/src/main/java/com/example/design_a_layout/MainActivity2.kt
@@ -1,11 +1,16 @@
 package com.example.design_a_layout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+    private  lateinit var image1 : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        image1 = findViewById(R.id.image1)
+        image1.setImageResource(R.drawable.gig)
     }
 }
+
